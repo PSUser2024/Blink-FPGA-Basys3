382087fe16a3f6da4d7c004d8e4bcb060ad32d14
Chris M <69206187+PSUser2024@users.noreply.github.com>
2026-08-17 20:42:55 -0400

    Added parameters for easier sim testing

--- src/rtl/top.v
+++ src/rtl/top.v
@@ -2,19 +2,24 @@
 // Top level: blinks led at ~1 Hz on a Basys3, with btnC as active-low reset.
 
 
-module top(
+module top #(
+    parameter int CLK_HZ = 100_000_000,
+    parameter int BLINK_HZ = 2
+)(
     input logic clk,
     input logic btnC,
     output logic [0:0] led
 );
 
-logic enable;
-logic nrst;
-
-assign nrst = ~btnC;
-
-enable_gen #(.DIVIDE(50000000)) enable_gen0 (.clk(clk), .nrst(nrst), .enable(enable));
-
-led_blink #() led_blink0 (.clk(clk), .nrst(nrst), .enable(enable), .led(led));
+    localparam int DIVIDE = CLK_HZ / (2*BLINK_HZ);
+    
+    logic enable;
+    logic nrst;
+    
+    assign nrst = ~btnC;
+    
+    enable_gen #(.DIVIDE(DIVIDE)) enable_gen0 (.clk(clk), .nrst(nrst), .enable(enable));
+    
+    led_blink #() led_blink0 (.clk(clk), .nrst(nrst), .enable(enable), .led(led));
 
 endmodule
--- src/sim/tb_top.sv
+++ src/sim/tb_top.sv
@@ -5,16 +5,23 @@ module tb_top();
     logic btnC = 1; //Input
     logic [0:0] led; //Output
     
-    top #() dut (.clk(clk), .btnC(btnC), .led(led));
+    localparam CLOCK_PERIOD_NS = 10;
+    localparam CLK_HZ = 1e9 / CLOCK_PERIOD_NS;
     
-    localparam CLOCK_PERIOD = 10;
-    always #(CLOCK_PERIOD / 2) clk = ~clk;
+    top #(.CLK_HZ(CLK_HZ), .BLINK_HZ(1e6)) dut (.clk(clk), .btnC(btnC), .led(led));
+
+    always #(CLOCK_PERIOD_NS / 2) clk = ~clk;
     
     initial begin
         repeat (5) @(posedge clk);
         btnC = 0;
+        $display("Initial Reset");
+        repeat (150) @(posedge clk);
+        btnC = 1;
+        repeat (20) @(posedge clk);
+        btnC = 0;
         $display("Reset Complete");
-        repeat (200000000) @(posedge clk);
+        repeat (100) @(posedge clk);
         $finish;
     end
     
